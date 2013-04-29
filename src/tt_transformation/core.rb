3e6e19aac68e49c29a7bf431570eb808bb0496dc
Thomas Thomassen <thomas@thomthom.net>
2013-04-29 22:22:40 +0200

    Updated TT_Lib guard and minor cleanups.

--- src/tt_transformation/core.rb
+++ src/tt_transformation/core.rb
@@ -9,83 +9,35 @@ require 'sketchup.rb'
 begin
   require 'TT_Lib2/core.rb'
 rescue LoadError => e
-  timer = UI.start_timer( 0, false ) {
-    UI.stop_timer( timer )
-    filename = File.basename( __FILE__ )
-    message = "#{filename} require TT_Lib² to be installed.\n"
-    message << "\n"
-    message << "Would you like to open a webpage where you can download TT_Lib²?"
-    result = UI.messagebox( message, MB_YESNO )
-    if result == 6 # YES
-      UI.openURL( 'http://www.thomthom.net/software/tt_lib2/' )
+  module TT
+    if @lib2_update.nil?
+      url = 'http://www.thomthom.net/software/sketchup/tt_lib2/errors/not-installed'
+      options = {
+        :dialog_title => 'TT_Lib² Not Installed',
+        :scrollable => false, :resizable => false, :left => 200, :top => 200
+      }
+      w = UI::WebDialog.new( options )
+      w.set_size( 500, 300 )
+      w.set_url( "#{url}?plugin=#{File.basename( __FILE__ )}" )
+      w.show
+      @lib2_update = w
     end
-  }
+  end
 end
 
 
 #-------------------------------------------------------------------------------
 
-if defined?( TT::Lib ) && TT::Lib.compatible?('2.6.0', 'Transformation Inspector')
+if defined?( TT::Lib ) && TT::Lib.compatible?( '2.7.0', 'Transformation Inspector' )
 
 module TT::Plugins::TransformationInspector
-  
-  
-  ### CONSTANTS ### ------------------------------------------------------------
-  
-  # Plugin information
-  PLUGIN_ID       = 'TT_Transformation'.freeze
-  PLUGIN_NAME     = 'Transformation Inspector'.freeze
-  PLUGIN_VERSION  = TT::Version.new(1,0,1).freeze
-  
-  # Version information
-  RELEASE_DATE    = '25 Apr 12'.freeze
-  
-  # Resource paths
-  PATH_ROOT   = File.dirname( __FILE__ ).freeze
-  PATH        = File.join( PATH_ROOT, 'TT_Transformation' ).freeze
-  
-  
-  ### VARIABLES ### ------------------------------------------------------------
-  
-  #@module_variable = 'Hello World'
-  
-  
+
   ### MENU & TOOLBARS ### ------------------------------------------------------
   
   unless file_loaded?( __FILE__ )
-    # Menus
     m = TT.menu( 'Plugins' )
     m.add_item( 'Transformation Inspector' ) { self.inspect_transformation }
-    
-    # Context menu
-    #UI.add_context_menu_handler { |context_menu|
-    #  model = Sketchup.active_model
-    #  selection = model.selection
-    #  # ...
-    #}
-    
-    # Toolbar
-    #toolbar = UI::Toolbar.new( PLUGIN_NAME )
-    #toolbar.add_item( ... )
-    #if toolbar.get_last_state == TB_VISIBLE
-    #  toolbar.restore
-    #  UI.start_timer( 0.1, false ) { toolbar.restore } # SU bug 2902434
-    #end
   end 
-  
-  
-  ### LIB FREDO UPDATER ### ----------------------------------------------------
-  
-  def self.register_plugin_for_LibFredo6
-    {   
-      :name => PLUGIN_NAME,
-      :author => 'thomthom',
-      :version => PLUGIN_VERSION.to_s,
-      :date => RELEASE_DATE,   
-      :description => 'Inspect and modify the transformation matrix directly.',
-      :link_info => 'http://forums.sketchucation.com/viewtopic.php?f=323&t=44859'
-    }
-  end
   
   
   ### MAIN SCRIPT ### ----------------------------------------------------------
@@ -106,7 +58,6 @@ module TT::Plugins::TransformationInspector
       :height       => height
     }
     
-    #@window ||= self.create_window
     @window = self.create_window( options )
     
     # Display the window on top of SketchUp's window.
@@ -272,16 +223,16 @@ module TT::Plugins::TransformationInspector
 
 
   
-  ### DEBUG ### ----------------------------------------------------------------
+  ### DEBUG ### ------------------------------------------------------------  
   
   # @note Debug method to reload the plugin.
   #
   # @example
   #   TT::Plugins::TransformationInspector.reload
   #
-  # @param [Boolean] tt_lib
+  # @param [Boolean] tt_lib Reloads TT_Lib2 if +true+.
   #
-  # @return [Integer]
+  # @return [Integer] Number of files reloaded.
   # @since 1.0.0
   def self.reload( tt_lib = false )
     original_verbose = $VERBOSE
@@ -290,16 +241,19 @@ module TT::Plugins::TransformationInspector
     # Core file (this)
     load __FILE__
     # Supporting files
-    x = Dir.glob( File.join(PATH, '*.{rb,rbs}') ).each { |file|
-      load file
-    }
-    x.length
+    if defined?( PATH ) && File.exist?( PATH )
+      x = Dir.glob( File.join(PATH, '*.{rb,rbs}') ).each { |file|
+        load file
+      }
+      x.length + 1
+    else
+      1
+    end
   ensure
     $VERBOSE = original_verbose
   end
-
-end
-
+  
+end # module
 
 end # if TT_Lib
 
